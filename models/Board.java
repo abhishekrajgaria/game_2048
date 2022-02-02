60a7fb14e8bac8c7935c8bc7c5dce5ec2fd8699c
abhrajga <abhishek.rajgaria@publicissapient.com>
2022-02-02 17:27:07 +0530

    added move functionality

--- models/Board.java
+++ models/Board.java
@@ -56,6 +56,31 @@ public class Board{
         return result;
     }
 
+    public void transpose(){
+        int[][] temp = new int[getSize()][];
+        for(int i=0;i<getSize();i++){
+            temp[i] = new int[getSize()];
+            for(int j=0;j<getSize();j++){
+                temp[i][j] = this.board[i][j];
+            }
+        }
+        for(int i=0;i<getSize();i++){
+            for(int j=0;j<getSize();j++){
+                this.board[i][j] = temp[j][i];
+            }
+        }
+    }
+
+    public void reverse(){
+        for(int x=0;x<=((getSize()/2)-1);x++){
+            for(int y=0;y<getSize();y++){
+                this.board[x][y] += this.board[getSize()-x-1][y];
+                this.board[getSize()-x-1][y] = this.board[x][y] - this.board[getSize()-x-1][y];
+                this.board[x][y] = this.board[x][y] - this.board[getSize()-x-1][y];
+            }
+        }
+    }
+
     public void printBoard(){
         System.out.println("******************************");
         for(int i=0;i<getSize();i++){
--- service/GameService.java
+++ service/GameService.java
@@ -1,62 +1,85 @@
 package service;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 import models.Board;
 import models.Cell;
 
-public class GameService{
+public class GameService {
 
-    public void addTile(Board board){
+    public void addTile(Board board) {
         Random rnd = new Random();
         List<Cell> emptyCells = board.emptyCells();
         Cell cell = emptyCells.get(rnd.nextInt(emptyCells.size()));
         board.setCell(cell, 2);
     }
 
-    public Board initializeBoard(int size){
+    public Board initializeBoard(int size) {
         Board board = new Board(size);
         addTile(board);
         addTile(board);
         return board;
     }
 
-    public Board resetBoard(Board board){
+    public Board resetBoard(Board board) {
         int size = board.getSize();
         return new Board(size);
     }
 
-    //aim to convert row to col
-    public void transpose(Board board){
-
-    }
-
-    //aim to get mirror image of board
-    public void reverse(Board board){
-
-    }
-
-    public void moveUp(Board board){
-
-    }
-
-    public void move(Board board, char dir){
-        if(dir=='L' || dir=='R'){
-            transpose(board);
+    public void moveUp(Board board) {
+        int newScore = 0;
+        for(int y=0;y<board.getSize();y++){
+            LinkedList<Integer> curCol = new LinkedList<>();
+            for(int x=0;x<board.getSize();x++){
+                Cell cell = new Cell(x,y);
+                if(!board.isEmpty(cell)){
+                    curCol.add(board.getCell(cell));
+                }
+            }
+            LinkedList<Integer> newCol = new LinkedList<>();
+            while(curCol.size()>=2){
+                int first = curCol.pop();
+                int second = curCol.peek();
+                if(second==first){
+                    newCol.add(first*2);
+                    newScore+=first*2;
+                    curCol.pop();
+                }
+                else{
+                    newCol.add(first);
+                }
+            }
+            newCol.addAll(curCol);
+            for(int x=0;x<board.getSize();x++){
+                Cell cell = new Cell(x,y);
+                if(newCol.isEmpty()){
+                    board.setCell(cell,0);
+                }
+                else{
+                    board.setCell(cell, newCol.pop());
+                }
+            }
         }
-        if(dir=='D' || dir=='R'){
-            reverse(board);
+        board.setScore(board.getScore()+newScore);
+    }
+
+    public void move(Board board, char dir) {
+        if (dir == 'L' || dir == 'R') {
+            board.transpose();
+        }
+        if (dir == 'D' || dir == 'R') {
+            board.reverse();
         }
         moveUp(board);
-        if(dir=='D' || dir=='R'){
-            reverse(board);
+        if (dir == 'D' || dir == 'R') {
+            board.reverse();
         }
-        if(dir=='L' || dir=='R'){
-            transpose(board);
+        if (dir == 'L' || dir == 'R') {
+            board.transpose();
         }
         board.printBoard();
     }
-
 
 }
