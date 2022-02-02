1b2b4e9e992a152e577461f4fa13aa819e5476e1
abhrajga <abhishek.rajgaria@publicissapient.com>
2022-02-02 12:37:58 +0530

    added models

---
+++ models/Board.java
@@ -0,0 +1,58 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board{
+    private int[][] board;
+    private int score;
+
+    public Board(int size){
+        this.board = new int[size][];
+        this.score = 0;
+
+        for(int i=0;i<size;i++){
+            this.board[i] = new int[size];
+            for(int j=0;j<size;j++){
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    public int getSize(){
+        return board.length;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    public int getCell(Cell cell){
+        return board[cell.getX()][cell.getY()];
+    }
+
+    public void setCell(Cell cell, int number){
+        board[cell.getX()][cell.getY()] = number;
+    }
+
+    public boolean isEmpty(Cell cell){
+        return getCell(cell)==0;
+    }
+
+    public List<Cell> emptyCells(){
+        List<Cell> result = new ArrayList<>();
+        for(int i=0;i<getSize();i++){
+            for(int j=0;j<getSize();j++){
+                Cell cell = new Cell(i,j);
+                if(isEmpty(cell)){
+                    result.add(cell);
+                }
+            }
+        }
+        return result;
+    }
+}
